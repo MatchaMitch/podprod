1f877382b497f25b5f33c25249ce7e09894c7571
Michael Decker <mjcdecker@gmail.com>
2020-05-28 11:02:43 +0200

    Built of new Split Template

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 import speech_recognition as sr
 import simpleaudio as sa
+from pydub import AudioSegment
+from pydub.playback import play
+from pydub.silence import split_on_silence
+
 
 app = Flask(__name__)
 
@@ -10,11 +14,14 @@ app.config["FILE_UPLOADS"] = "/Users/Micha/documents/github/podprod/uploads"
 app.config["ALLOWED_EXTENSIONS"] = ["WAV"]
 app.config["MAX_FILESIZE"] = 86400000
 
+# Function check for right file extension
 def allowed_file(filename):
 
+	# Check if file has an extension
 	if not "." in filename:
 		return False
 
+	# Check if File has allowed extension
 	ext = filename.rsplit(".", 1)[1]
 
 	if ext.upper() in app.config["ALLOWED_EXTENSIONS"]:
@@ -22,6 +29,7 @@ def allowed_file(filename):
 	else:
 		return False
 
+# Function check of allowed file size
 def allowed_filesize(filesize):
 
 	if int(filesize) <= app.config["MAX_FILESIZE"]:
@@ -29,52 +37,13 @@ def allowed_filesize(filesize):
 	else:
 		return False
 
+# Google Speech API Key
 GOOGLE_SPEECH_API_KEY = None
 
+# Flask route to homepage
 @app.route("/")
 def index():
 	return render_template("index.html")
-
-
-@app.route("/upload2", methods=["GET", "POST"])
-def upload_file():
-
-	if request.method == "GET":
-		return render_template('upload.html')
-
-	if request.method == "POST":
-
-		if request.files:
-
-			if not allowed_filesize(request.cookies.get("filesize")):
-				print("File exceeded maximum size")
-				return redirect(request.url)
-
-			file = request.files["file"]
-
-			if file.filename == "":
-				print("File must have a filename")
-				return redirect(request.url)
-
-			if not allowed_file(file.filename):
-				print("Your file doesn't have the right format")
-				return redirect(request.url)
-
-			else:
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config["FILE_UPLOADS"], filename))
-
-				# Speech Recognition stuff.
-				recognizer = sr.Recognizer()
-				audio_file = sr.AudioFile(file)
-				with audio_file as source:
-					audio_data = recognizer.record(source)
-				text = recognizer.recognize_google(audio_data, key=GOOGLE_SPEECH_API_KEY, language="en-EN")
-				print(text)
-
-			print("file saved")
-
-			return redirect(request.url)
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
@@ -115,7 +84,36 @@ def upload():
 			filepath = os.path.join(app.config["FILE_UPLOADS"], filename)
 			file.save(filepath)
 
+			# Play the sound
+			sound = AudioSegment.from_file(file, format="wav")
+			play(sound)
+
 		return render_template('upload.html', extra_line=extra_line)
+
+@app.route("/split", methods=["GET", "POST"])
+def split():
+
+	entries = []
+		
+	# Open a file
+	path = app.config["FILE_UPLOADS"]
+	
+	with os.scandir(path) as dirs:
+		for entry in dirs:
+			entries.append(entry.name)
+
+		
+
+	if request.method == "POST":
+		split_file = request.form['split_file']
+		print(split_file)
+
+		return render_template('split.html', entries=entries)
+
+	else:
+		return render_template('split.html', entries=entries)
+	
+
 
 if __name__ == "__main__":
 	app.run()
