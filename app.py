820f1be0a7ba2811ad4c936e243805decf97b46c
Michael Decker <mjcdecker@gmail.com>
2020-06-08 11:44:21 +0200

    Built in Database, Login & Register Feature

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, jsonify
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
 import speech_recognition as sr
 import simpleaudio as sa
+
 from pydub import AudioSegment
 from pydub.playback import play
 from pydub.silence import split_on_silence
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 import numpy as np
 import wave
 import sys
 
+from flask_session import Session
+from cs50 import SQL
+from tempfile import mkdtemp
+
+from helpers import apology, login_required, lookup, usd
+
+
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///podprod.db")
 
 app.config["FILE_UPLOADS"] = "/Users/Micha/documents/github/podprod/uploads"
 app.config["IMG_UPLOADS"] = "/Users/Micha/documents/github/podprod/static/img"
@@ -52,6 +88,7 @@ def index():
 	return render_template("index.html")
 
 @app.route("/upload", methods=["GET", "POST"])
+@login_required
 def upload():
 
 	if request.method == "GET":
@@ -121,6 +158,7 @@ def upload():
 		return render_template('upload.html')
 
 @app.route("/remove-silence", methods=["GET", "POST"])
+@login_required
 def remove_silence():
 	entries = []
 		
@@ -199,6 +237,7 @@ def remove_silence():
 
 
 @app.route("/transcribe", methods=["GET", "POST"])
+@login_required
 def transcribe():
 
 	text = ''
@@ -229,6 +268,99 @@ def transcribe():
 	else:
 		return render_template('transcribe.html', entries=entries, text=text)
 	
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password", 403)
+
+        # Ensure password-confirmation was submitted
+        elif not confirmation:
+            return apology("must provide password", 403)
+
+        # Ensure passwords & password check are the same
+        elif not password == confirmation:
+            return apology("the passwords must be identical", 403)
+
+        # Ensure username is not yet in database
+        namecheck = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=username)
+
+        if len(namecheck) == 1:
+            return apology(" username already exists", 403)
+
+        #hash the password
+        hash = generate_password_hash(password)
+
+        # Insert user into database
+        rows = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=username, hash=hash)
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 if __name__ == "__main__":
 	app.run(debug=True)
